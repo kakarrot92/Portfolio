a0df7ada0c10e75acc74dcf835930b2c1f42a37e
kakarrot92 <swiftblade1992@gmail.com>
2021-11-10 18:32:13 +0100

    Add popup project card

--- app.js
+++ app.js
@@ -1,9 +1,71 @@
+import data from './data.js';
 const slideMenu = () => {
   const hamburger = document.getElementById('hamburger');
   const navLinks = document.getElementById('nav-list');
+  const seeProject = document.querySelectorAll('.button');
+  const closeModal = document.querySelector('.close-modal');
+  const sectionOne = document.querySelector('.modal')
 
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('nav-active');
+  });
+
+  [...seeProject].forEach((elem) => {
+    elem.addEventListener('click', (e) => {
+      const myData = data.find((elem) => elem.id === e.currentTarget.dataset.btnId);
+      const {
+        url, tittle, technologi1, technologi2, technologi3, text,
+      } = myData;
+      
+      let section = document.createElement('section')
+      section.className = 'modal';
+      section.innerHTML = `
+      <div class="card-modal">
+          <div class="modal-body">
+          <img src="./images/close-butt.svg" class="close-modal" id="close-modal" alt="close button">
+            <img src="${url}" class="popup-img">
+          <div class= "flex-start">
+            <div class= "desktop-header">
+              <div class="desktop-h2">
+                <h2 class="modal-header">${tittle}</h2>
+              </div
+              <div class="desktop-btn">
+                <button class="button-modal-desktop" type="button">
+                  <a href="https://github.com/kakarrot92" target="_blank">See Live <img src="images/live-icon.svg" alt="live-icon for my project"></a>
+                 </button>
+                <button class="button-modal-desktop" type="button">
+                  <a href="https://github.com/kakarrot92" target="_blank">See Live <img src="images/Vector.png" alt="live-icon for my github"></a>
+                </button>
+              </div>
+            </div>
+            <div class="technologys">
+              <ul>
+                <li>${technologi1}</li>
+                <li>${technologi2}</li>
+                <li>${technologi3}</li>
+              </ul>
+              <p class="modal-text">${text}</p>
+            </div>
+              <div class="modal-btn">
+                <button class="button-modal" type="button">
+                    <a href="https://github.com/kakarrot92" target="_blank">See Live <img src="images/live-icon.svg" alt="live-icon for my project"></a>
+                </button>
+                <button class="button-modal" type="button">
+                  <a href="https://github.com/kakarrot92" target="_blank">See Live <img src="images/Vector.png" alt="live-icon for my github"></a>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+      document.querySelector('body').insertAdjacentElement('beforeend', section)
+      // puut your function for close button below
+      closeModal.addEventListener('click', () => {
+        document.querySelector('.modal').remove()
+      })
+      
+    });
   });
 
   navLinks.addEventListener('click', (event) => {
---
+++ popup.js
@@ -0,0 +1,29 @@
+// import data from './data.js';
+
+// const body = document.querySelector('body');
+// const project = data[0];
+// const section = document.createElement('section');
+// const card = document.createElement('div');
+// const img = document.createElement('img');
+// const h2 = document.createElement('h2');
+// const ul = document.createElement('ul');
+// const liOne = document.createElement('li');
+// const liTwo = document.createElement('li');
+// const liThree = document.createElement('li');
+// const p = document.createElement('p');
+// const btnDiv = document.createElement('div');
+// const button1 = document.createElement('button');
+// const button2 = document.createElement('button');
+
+// body.append(section);
+// section.append(card);
+// card.append(img);
+// card.append(h2);
+// card.append(ul);
+// ul.append(liOne);
+// ul.append(liTwo);
+// ul.append(liThree);
+// card.append(p);
+// section.append(btnDiv);
+// btnDiv.append(button1);
+// btnDiv.append(button2);
