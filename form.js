d84c9dfc2846c3f5141a9c378001aefa08100607
kakarrot92 <swiftblade1992@gmail.com>
2021-11-12 16:21:49 +0100

    Add form local storage

--- form.js
+++ form.js
@@ -1,14 +1,49 @@
 const form = document.getElementById('contact-form');
-const email = document.getElementById('email');
+const emailForm = document.getElementById('email');
 const validateMessage = document.getElementById('validateMessage');
+const userName = document.getElementById('full-name');
+const text = document.getElementById('text');
 
 form.addEventListener('submit', (e) => {
   // const userEmail = email.value.trim();
 
-  if (email.value !== email.value.toLowerCase()) {
+  if (emailForm.value !== emailForm.value.toLowerCase()) {
     e.preventDefault();
     validateMessage.textContent = 'Email is not valid';
   } else {
     validateMessage.textContent = '';
   }
 });
+
+const dataStorage = {
+  name: '',
+  email: '',
+  text: '',
+};
+
+function saveData() {
+  dataStorage.email = emailForm.value;
+  dataStorage.name = userName.value;
+  dataStorage.text = text.value;
+  localStorage.setItem('dataStorage', JSON.stringify(dataStorage));
+}
+
+userName.addEventListener('input', () => {
+  saveData();
+});
+
+emailForm.addEventListener('input', () => {
+  saveData();
+});
+
+text.addEventListener('input', () => {
+  saveData();
+});
+
+function storeData() {
+  const dataStored = JSON.parse(localStorage.getItem('dataStorage'));
+  userName.value = dataStored.name;
+  emailForm.value = dataStored.email;
+  text.value = dataStored.text;
+}
+storeData();
